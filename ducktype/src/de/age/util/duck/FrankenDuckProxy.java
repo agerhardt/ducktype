41db26ded760a1231e57945cbf0c137474d47173
Alexander Gerhardt <h.a.gerhardt@googlemail.com>
2012-11-16 06:47:39 +0100

    moved proxy to new file so i can clean up

---
+++ ducktype/src/de/age/util/duck/FrankenDuckProxy.java
@@ -0,0 +1,37 @@
+package de.age.util.duck;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+public class FrankenDuckProxy implements InvocationHandler {
+
+	private final Object[] targetObjects;
+	
+	private FrankenDuckProxy(Object ... targetObjects) {
+		this.targetObjects = targetObjects;
+	}
+	
+	@Override
+	public Object invoke(Object proxy, Method method, Object[] args)
+			throws Throwable {
+		for (Object possibleTarget : targetObjects) {
+			try {
+				Method targetMethod = possibleTarget.getClass().getMethod(method.getName(), method.getParameterTypes());
+				return targetMethod.invoke(possibleTarget, args);
+			} catch (NoSuchMethodException exc) {
+				continue;
+			}
+		}
+		throw new MissingMethodException(method);
+	}
+
+	@SuppressWarnings("unchecked")
+	public static <D> D createProxy(Class<D> ducktypeClass, Object ... targetObjects) {
+		Class<?>[] interfaces = new Class<?>[] { ducktypeClass };
+		ClassLoader classLoader = ducktypeClass.getClassLoader();
+		FrankenDuckProxy handler = new FrankenDuckProxy(targetObjects);
+		return (D) Proxy.newProxyInstance(classLoader, interfaces, handler); 
+	}
+	
+}
